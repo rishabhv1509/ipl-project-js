8fc72dd72af01802e12de30ccc7637c78083d655
rishabhv1509 <rishabhv1509@gmail.com>
2019-04-08 07:40:44 +0530

    sixth commit
    added charts and server

---
+++ src/js/index.js
@@ -0,0 +1,140 @@
+function matchCountPlot (matchCountData) {
+    let seasons = Object.keys(matchCountData)
+    let matchData = formatData(seasons, matchCountData)
+    
+    var myChart = Highcharts.chart('matchCount', {
+        chart: {
+            type: 'column'
+          },
+          title: {
+            text: 'IPL Seasons : Total Matches Played'
+          },
+          xAxis: {
+            type: 'category'
+          },
+          yAxis: {
+            title: {
+              text: 'Total Matches'
+            }
+          },
+          legend: {
+            enabled: false
+          },
+          plotOptions: {
+            series: {
+              borderWidth: 0
+            }
+          },
+      
+          'series': [
+            {
+              'name': 'Total Matches',
+              'colorByPoint': true,
+              'data': matchData
+        }]
+    });
+  }
+  
+    function extrasCountPlot (extrasData) {
+      let teamNames = Object.keys(extrasData)
+      let extrasCountData = formatData(teamNames, extrasData)
+      Highcharts.chart('extrasCount', {
+        chart: {
+          type: 'column'
+          },
+        title: {
+            text: 'IPL Season 2016 : ExtraRuns Conceived'
+          },
+        xAxis: {
+            type: 'category'
+          },
+        yAxis: {
+            title: {
+              text: 'Total Extras'
+            }
+          },
+        legend: {
+            enabled: false
+          },
+        plotOptions: {
+            series: {
+              borderWidth: 0
+            }
+          },
+          'series': [
+            {
+              'name': 'Total Extras',
+              'colorByPoint': true,
+              'data': extrasCountData
+            }
+          ]
+        })
+      }
+
+      function economicalBowlersPlot (ecoBowlerData) {
+        let bowlerEconomyData = Object.keys(ecoBowlerData)
+        let topEconomicalBowlers = formatData(bowlerEconomyData, ecoBowlerData)
+        
+        Highcharts.chart('economicalBowlers', {
+          chart: {
+            type: 'column'
+          },
+          title: {
+            text: 'IPL Season 2015 : Top Economical Bowlers'
+          },
+          xAxis: {
+            type: 'category'
+          },
+          yAxis: {
+            title: {
+              text: 'Economy'
+            }
+          },
+          legend: {
+            enabled: false
+          },
+          plotOptions: {
+            series: {
+              borderWidth: 0
+            }
+          },
+      
+          tooltip: {
+            headerFormat: '<span style="font-size:13px">{series.name}</span><br>',
+            pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y}</b><br/>'
+          },
+      
+          'series': [
+            {
+              'name': 'Top Economical Bowlers',
+              'colorByPoint': true,
+              'data': topEconomicalBowlers
+            }
+          ]
+        })
+      }
+
+      function formatData (requiredData, givenData) {
+
+        return requiredData.reduce((dataObjectArr, dataName) => {
+          var dataObject = {}
+          dataObject['name'] = dataName
+          dataObject['y'] = givenData[dataName]
+          dataObjectArr.push(dataObject)
+          return dataObjectArr
+        }, [])}
+
+$.getJSON("../../json_files/matchesPerYear.json", function(json) {
+    console.log(json);
+    matchCountPlot(json)
+});
+
+$.getJSON("../../json_files/extraRuns.json", function(json) {
+    console.log(json);
+    extrasCountPlot(json)
+});
+
+$.getJSON("../../json_files/ecoBowlers.json", function(json) {
+    console.log(json);
+    economicalBowlersPlot(json)
+});
